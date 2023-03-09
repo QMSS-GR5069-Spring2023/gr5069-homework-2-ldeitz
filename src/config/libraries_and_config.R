219c33cc89bc14a505205d0df9d75eba804647cb
sarangpmr <sarpatel095@gmail.com>
2023-03-09 11:38:04 -0500

    Add script to load packages and set wd

---
+++ src/config/libraries_and_config.R
@@ -0,0 +1,26 @@
+## ---------------------------
+##
+## Script name: libraries_and_config
+##
+## Purpose of script: To load necessary scripts and set the working directory
+##
+## Author: Sarang Patel
+##
+## Date Created: 2023-03-08
+##
+## ---------------------------
+##
+## Notes: 
+##   This script can be used to set the working directory for both Mac and PC.
+##
+## ---------------------------
+
+## set working directory for Mac and PC
+
+setwd("~/gr5069-homework-2-ldeitz/")
+
+## ---------------------------
+require(babynames)
+require(ggplot2)
+require(magrittr)
+require(dplyr)
